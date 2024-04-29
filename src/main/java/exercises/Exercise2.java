f7dfa1e4f03fdfef1740d9981ea3acd3eb78626d
Spiersy95 <scottspeirsstirling@gmail.com>
2024-04-29 11:30:34 +0100

    Finished exercise 2

--- src/main/java/exercises/Exercise2.java
+++ src/main/java/exercises/Exercise2.java
@@ -6,11 +6,17 @@ import java.util.Scanner;
 public class Exercise2 {
 
     public static int[] getUserInput() {
-
         while (true) {
-            Scanner input = new Scanner(System.in);
             try{
-
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter divisor: ");
+                int a = input.nextInt();
+                System.out.println("Enter dividend: ");
+                int b = input.nextInt();
+                input.close();
+                return new int[]{a, b};
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again.");
             }
         }
 
