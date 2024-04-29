d6972e5b8f4d41524d7f2b39961bc90879427690
Sunny <Sunnylaw3571@gmail.com>
2024-04-29 15:11:49 +0100

    complete task 6

--- src/main/java/exercises/Main.java
+++ src/main/java/exercises/Main.java
@@ -1,15 +1,15 @@
-import exercises.Exercise4;
+import exercises.Exercise5;
 
 public class Main {
 
     public static void main(String[] args) {
 //        Exercise3.divide();
-        try {
-            Exercise4.recursiveMethod();
-        } catch (StackOverflowError e){
-            System.out.println("Oops, we're out of space on the stack. Goodnight.");
-        }
-
+//        try {
+//            Exercise4.recursiveMethod();
+//        } catch (StackOverflowError e){
+//            System.out.println("Oops, we're out of space on the stack. Goodnight.");
+//        }
+        Exercise5.healthInsuranceDataInput();
     }
 }
 
