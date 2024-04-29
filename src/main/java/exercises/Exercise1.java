ae5fbec073c2e1ccdec9c80f609bc4e47496081f
Spiersy95 <scottspeirsstirling@gmail.com>
2024-04-29 13:44:46 +0100

    Completed exercise4

--- src/main/java/exercises/Exercise1.java
+++ src/main/java/exercises/Exercise1.java
@@ -1,7 +1,11 @@
 package exercises;
 
 public class Exercise1 {
-    public static int divide(int a, int b){
+    public static int divide(int a, int b) throws NegativeIntegerInputException {
+        if (a < 0 || b < 0){
+            throw new NegativeIntegerInputException();
+        }
+
         return a/b;
     }
 
